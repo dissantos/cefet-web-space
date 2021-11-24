0bc032eff400728a2d97930556c86700fbbcdd4a
dissantos <disantosg18@gmail.com>
2021-11-24 00:01:35 -0300

    - Add law of gravitation

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,18 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let calcularBotaoEl = document.querySelector('#calcular');
+
+calcularBotaoEl.addEventListener('click', () => {
+    const const_grav = document.querySelector('#constante').value;
+
+    let massa1 = document.querySelector('#massa1').value;
+    let massa2 = document.querySelector('#massa2').value;
+    let distancia = document.querySelector('#distancia').value;
+    
+    let resultado = const_grav*massa1*massa2/(Math.pow(distancia,2));
+    
+    let resultadoElement = document.querySelector('#resultado');
+    
+    resultadoElement.value = resultado.toExponential(3);
+})
